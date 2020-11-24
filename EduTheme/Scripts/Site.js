06c2ae3a6c42d0a1b3c67f2f37f85a4b753b4ba0
Hatem <hatem_eskisehir@hotmail.com>
2020-11-24 16:46:15 +0300

    navbar edited.

--- EduTheme/Scripts/Site.js
+++ EduTheme/Scripts/Site.js
@@ -1,1 +1,12 @@
-﻿
+﻿//Change pos/background/padding/add shadow on nav when scroll event happens 
+$(function () {
+	var navbar = $('.navbar');
+
+	$(window).scroll(function () {
+		if ($(window).scrollTop() <= 40) {
+			navbar.removeClass('navbar-scroll');
+		} else {
+			navbar.addClass('navbar-scroll');
+		}
+	});
+});
