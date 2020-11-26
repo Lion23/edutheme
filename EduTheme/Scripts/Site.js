061e801c3a15a5e607828b1de76afe043d84ff1e
Hatem <hatem_eskisehir@hotmail.com>
2020-11-26 14:14:16 +0300

    logo added

--- EduTheme/Scripts/Site.js
+++ EduTheme/Scripts/Site.js
@@ -2,11 +2,17 @@
 $(function () {
 	var navbar = $('.navbar');
 
+	var logo_img = $('.navbar-brand img');
+
+
 	$(window).scroll(function () {
 		if ($(window).scrollTop() <= 40) {
 			navbar.removeClass('navbar-scroll');
+			logo_img.removeClass('scroll-down-img');
+
 		} else {
 			navbar.addClass('navbar-scroll');
+			logo_img.addClass('scroll-down-img');
 		}
 	});
 });
